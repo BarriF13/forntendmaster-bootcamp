77065fe4cb3d63b34f1e5fe4b398cd60c005a8eb
Barri <info@bdesignstudio.co.uk>
2021-02-25 12:31:12 +0000

    const let var

---
+++ js-with-brian-holt/dayOne/app.js
@@ -0,0 +1,12 @@
+// const monthlyRent = 400;
+
+// const MONTHS_IN_A_YEAR = 12;
+
+// const yearRent = monthlyRent * MONTHS_IN_A_YEAR;
+// console.log(yearRent);
+
+// const firstName= 'Barri';
+// const lastName= 'Faryad'
+// const sentence = `Hello ${firstName} ${lastName}, we are revising js again lol`;
+// console.log(sentence)
+
