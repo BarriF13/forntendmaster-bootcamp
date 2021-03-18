e78b3cf8ec5726fc3297630f7741cbee0c98c01b
Barri <info@bdesignstudio.co.uk>
2021-03-18 19:29:30 +0000

    initial project for swiper image gallery

---
+++ image-gallery-2/app.js
@@ -0,0 +1,9 @@
+new Swiper('.swiper-container', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 100,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
