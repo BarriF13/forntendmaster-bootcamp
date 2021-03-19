523198da43562615099a5d5b0b7f516af056b115
Barri <info@bdesignstudio.co.uk>
2021-03-19 11:37:09 +0000

    add spinner£

--- api-dogs/app.js
+++ api-dogs/app.js
@@ -18,40 +18,45 @@
 // document.querySelector('.add-doggo').addEventListener('click', addDoggo)
 const BREED_URL = 'https://dog.ceo/api/breeds/list/all';
 
-function addDoggo() {
+const select = document.querySelector('.breeds');
+const spinner = document.querySelector('.spinner');
+fetch(BREED_URL)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    //console.log(data);
+    //console.log(Object.keys(data.message));
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+    // console.log(breedsArray);
+    // const select = document.querySelector('.breeds');
+    for (i = 0; i < breedsArray.length; i++) {
 
-  fetch(BREED_URL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      //console.log(data);
-      //console.log(Object.keys(data.message));
-      const breedsObject = data.message;
-      const breedsArray = Object.keys(breedsObject);
-      // console.log(breedsArray);
-      const select = document.querySelector('.breeds');
-      for (i =0; i< breedsArray.length; i++) {
-        const option = document.createElement('option');
-        option.value = breedsArray[i];
-        option.innerText = breedsArray[i];
-        select.appendChild(option)
-      }
-    });
+      const option = document.createElement('option');
+      option.value = breedsArray[i];
+      option.innerText = breedsArray[i];
+      select.appendChild(option) 
+    }
+  });
 
+
+select.addEventListener('change', (e) => {
+  let url = `https://dog.ceo/api/breed/${e.target.value}/images/random`;
+  getDoggo(url);
+})
+
+
+const img = document.querySelector('.dog-img');
+
+function getDoggo(url){
+  spinner.classList.add('show');
+  img.classList.remove('show');
+fetch(url).then((res)=>res.json())
+.then((data)=> {
+img.src = data.message;
+spinner.classList.remove('show');
+img.classList.add('show');
+
+})
 }
-document.querySelector('.add-doggo').addEventListener('click', addDoggo)
-
-
-
-// const CAT_URL = "https://api.thecatapi.com/v1/images/search"
-// fetch(CAT_URL)
-// .then((res)=>{
-// return res.json()
-// }).then((data)=>{
-//   console.log(Object.keys(data));
-//   const img = document.createElement('img');
-// img.src= data.url;
-// img.alt= 'cat';
-// document.querySelector('.cats').appendChild(img)
-// });
